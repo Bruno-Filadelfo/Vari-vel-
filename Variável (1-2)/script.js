04846fdeb2c8659cb33f0a19ce12962edeab0471
Bruno Carvalho Filadelfo <Bruno.Filadelfo@Outlook.Com>
2021-10-13 02:00:08 -0300

    Add files via upload

---
+++ Variável (1-2)/script.js
@@ -0,0 +1,16 @@
+// string 1 
+
+var nome = "Bruno";  
+var sobrenome = "Carvalho";
+var idade = "25";
+var altura = "1,78";
+var habilitação = "A e B";
+
+nome = prompt("Qual é o primeiro nome?");
+sobrenome = prompt("Qual é o seu sobronome?");
+idade = prompt("Qual a sua idade?");
+altura = prompt ("Qual a sua altura?");
+habilitação = prompt ("Tem habilitação?");
+qual = prompt ("Qual?");
+
+alert("Seja bem Vindo!");
